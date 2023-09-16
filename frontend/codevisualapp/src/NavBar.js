0fa8309cd459036e7089c83a0a34e353cd90206c
Henry Nguyen <henryvnguyen57@gmail.com>
2023-09-16 12:54:11 -0500

    added navbar and styling

--- frontend/codevisualapp/src/NavBar.js
+++ frontend/codevisualapp/src/NavBar.js
@@ -1,12 +1,33 @@
-import {Link} from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import './NavBar.css'
 
-const NavBar = () => {
-    return(
-        <ul>
-          <li><Link to="/dragdrop">Upload Folder/File</Link></li>
-          <li><Link to="/visual">Visual</Link></li>
-        </ul>
+export default function NavBar() {
+    return (
+        <nav className="nav">
+            <Link to="/" >
+                <p className="navbar_title">Put Title Here</p>
+            </Link>
+
+            {/* <Link to="/" className="navbar_title">
+                TechTutor
+            </Link> */}
+            <ul>
+                <CustomLink className='navbar_chat' to="/dragdrop">Upload</CustomLink>
+                <CustomLink className='navbar_prompt' to="/visual">Visualizer</CustomLink>
+            </ul>
+        </nav>
     )
 }
 
-export default NavBar
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    )
+}
